fe5c722d5ca60d087f0b051ba871eb128b40ab24
Manish Kumar Gupta <44649787+YourManishGupta@users.noreply.github.com>
2021-02-02 23:54:16 +0530

    Create 183 - Customers who never order.sql

---
+++ Database/183 - Customers who never order.sql
@@ -0,0 +1,41 @@
+/*
+
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+
+-- 1
+SELECT Name AS Customers FROM Customers WHERE Id NOT IN
+(SELECT CustomerId FROM Orders);
+
+-- 2
+select Name as Customers from Customers as c 
+left join orders as o on c.id = o.CustomerId 
+where o.CustomerId is null;
